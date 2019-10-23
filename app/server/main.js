f727c04be63cfd5118b4f222634be54d8365939b
Chris Won <wonc@hawaii.edu>
2019-10-22 20:05:28 -1000

    Finished E42

---
+++ app/server/main.js
@@ -0,0 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+import { people } from '../both/PeopleCollection';
+import './PeopleInitialization';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
